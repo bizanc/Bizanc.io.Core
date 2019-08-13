1b3d26be8514fbf97c696f85e32ca9f0fdb3dcba
Leandro Lustosa <leandro.lustosa.s@gmail.com>
2019-08-13 20:25:29 -0300

    fix before install

--- Bizanc.io.Matching.App/before-install.sh
+++ Bizanc.io.Matching.App/before-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-/bin/bash mkdir /datadrive/backup
-/bin/bash mv /datadrive/seednode/* /datadrive/backup
-/bin/bash mv /datadrive/backup/RavenDB /datadrive/seednode/RavenDB 
-/bin/bash rm -rf /datadrive/backup
+/bin/bash "mkdir /datadrive/backup"
+/bin/bash "mv /datadrive/seednode/* /datadrive/backup"
+/bin/bash "mv /datadrive/backup/RavenDB /datadrive/seednode/RavenDB"
+/bin/bash "rm -rf /datadrive/backup"
