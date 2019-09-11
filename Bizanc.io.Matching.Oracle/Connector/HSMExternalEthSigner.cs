5f17fd340c77cf1152806c1ea647f5d188ee719e
Leandro Lustosa <leandro.lustosa.s@gmail.com>
2019-09-11 17:28:06 -0300

    Remove public key prefix

--- Bizanc.io.Matching.Oracle/Connector/HSMExternalEthSigner.cs
+++ Bizanc.io.Matching.Oracle/Connector/HSMExternalEthSigner.cs
@@ -27,7 +27,7 @@ namespace Bizanc.io.Matching.Infra.Connector
 
         protected override async Task<byte[]> GetPublicKeyAsync()
         {
-            return await Task.FromResult(CryptoHelper.StringToByteArray("040d701643c7c8cf247e37f8f20b315f588bec69bf3d71ef4d5f53f379ef16246e0150013aee41d6b344b1313f7ac811ed3d14d3e97b647831bf52e9eb9a2a321f"));
+            return await Task.FromResult(CryptoHelper.StringToByteArray("0d701643c7c8cf247e37f8f20b315f588bec69bf3d71ef4d5f53f379ef16246e0150013aee41d6b344b1313f7ac811ed3d14d3e97b647831bf52e9eb9a2a321f"));
                                                                                                                                                     
         }
 
