c8be0f099db39d30c48c5d5cee287c62c0662208
Leandro Lustosa <leandro.lustosa.s@gmail.com>
2020-01-09 17:53:12 -0300

    Update RavenDB Framework Version

--- Bizanc.io.Matching.Infra/Repository/BaseRepository.cs
+++ Bizanc.io.Matching.Infra/Repository/BaseRepository.cs
@@ -21,7 +21,7 @@ namespace Bizanc.io.Matching.Infra.Repository
             {
                 started = true;
                 
-                EmbeddedServer.Instance.StartServer(new ServerOptions(){ FrameworkVersion= "2.2.7" });
+                EmbeddedServer.Instance.StartServer(new ServerOptions(){ FrameworkVersion= "2.2.8" });
 #if DEBUG
                 EmbeddedServer.Instance.OpenStudioInBrowser();
 #endif
